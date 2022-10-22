ee6d4bc48f3576ec3deeb65d48fb81e1d66b60ab
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-10-21 22:38:21 -0400

    🚧 Finalizando aula 21/10

--- src/js/dom-parte2.js
+++ src/js/dom-parte2.js
@@ -14,6 +14,22 @@ const divPai = document.querySelector('#elemento-pai')
 const divFilho1 = document.querySelector('#elemento-filho1')
 const divFilho2 = document.querySelector('#elemento-filho2')
 
+divPai.addEventListener('click', (event) => {
+    console.log(`
+        Click no elemento Pai: Disparado por `, event.target)        
+})
+divFilho1.addEventListener('click', (event)=>{
+    event.stopImmediatePropagation()
+    console.log(`
+    Click no elemento Filho 1: Disparado por `, event.target)  
+
+
+})
+divFilho2.addEventListener('click', (event)=>{
+    event.stopImmediatePropagation()
+    console.log(`
+    Click no elemento Filho 2: Disparado por `, event.target)  
+})
 
 
 
@@ -27,7 +43,36 @@ const selectEmoji = formFabricaEmojis.emoji
 const btnCriarEmoji = formFabricaEmojis.criarEmoji
 const listaEmojis = document.querySelector('#listaEmojis')
 
+emojis.forEach((emoji)=>{
+    let option = document.createElement('option')
+    option.value = emoji.icone
+    option.textContent = `${emoji.icone} ${emoji.descricao}`
+    selectEmoji.appendChild(option)
+})
+
+btnCriarEmoji.addEventListener('click',()=>{
+    //let emojiSelecionado = document.createElement('div')
+   // emojiSelecionado.textContent = selectEmoji.value
+    //listaEmojis.appendChild(emojiSelecionado)
+    let emojiSelecionado = `
+    <div class="emoji">
+    ${selectEmoji.value}
+    <div/>
+    `
+    listaEmojis.innerHTML+=emojiSelecionado
+})
+
+listaEmojis.addEventListener('dblclick',function(event){
+    let elemento = event.target
+
+    if(elemento.className === "emoji"){
+        listaEmojis.removeChild(elemento)
+    }
+})
+
+
 
 // Tópico 2 : Eventos de Teclado
 
 const infoTeclas = document.querySelector('#infoTeclas')
+
